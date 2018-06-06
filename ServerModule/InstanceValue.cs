114f6847cbed99d9a9c1928065ea9630649c1a3a
ChoGwanHee <enycal@gmail.com>
2018-06-07 03:05:34 +0900

    Management scripts function implementation

--- ServerModule/InstanceValue.cs
+++ ServerModule/InstanceValue.cs
@@ -2,10 +2,121 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerModule
 {
-    class InstanceValue
+    public enum PeerValue
     {
+        Disconnected,
+        Connected,
+        Disconnecting,
+        Connecting,
+        RoomIn,
+        RoomOut,
+        Ready,
+        Win,
+        Lose
+    }
+
+    public class InstanceValue
+    {
+        /// <summary>
+        /// server argument
+        /// </summary>
+        private Socket _tcp;
+        public Socket TCP
+        {
+            get { return _tcp; }
+            set { _tcp = value; }
+        }
+
+        private UdpClient _udp;
+        public UdpClient UDP
+        {
+            get { return _udp; }
+            set { _udp = value; }
+        }
+
+        private string _address;
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        private int _port;
+        public int Port
+        {
+            get { return _port; }
+            set { _port = value; }
+        }
+
+        private byte[] _buffersize;
+        public byte[] BufferSize
+        {
+            get { return _buffersize; }
+            set { _buffersize = value; }
+        }
+
+        private int _receive;
+        public int Receive
+        {
+            get { return _receive; }
+            set { _receive = value; }
+        }
+
+        /// <summary>
+        /// lobby argument
+        /// </summary>
+        private bool _connected;
+        public bool Connected
+        {
+            get { return _connected; }
+            set { _connected = value; }
+        }
+
+        private int _id;
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private int _state;
+        public int State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        private string _version;
+        public string Version
+        {
+            get { return _version; }
+            set { _version = value; }
+        }
+
+        private string _nickname;
+        public string Nickname
+        {
+            get { return _nickname; }
+            set { _nickname = value; }
+        }
+
+        private int _min;
+        public int Min
+        {
+            get { return _min; }
+            set { _min = value; }
+        }
+
+        private int _max;
+        public int Max
+        {
+            get { return _max; }
+            set { _max = value; }
+        }
     }
 }
--- ServerModule/ServerManager.cs
+++ ServerModule/ServerManager.cs
@@ -3,9 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+// http://treeofimaginary.tistory.com/96 .NET exception 버그 참고
+// Unity\Editor\Data\PlaybackEngines\windowsstandalonesupport\Variations\win32_development_mono\Data\Managed
 namespace ServerModule
 {
-    class ServerManager
+    public class ServerManager : InstanceValue
     {
+        public static InstanceValue Instance = new InstanceValue();
+        private static ServerManager _server;
+        public static ServerManager Server
+        {
+            get { return _server; }
+            set { _server = value; }
+        }
+
+        private void Start()
+        {
+            SetConnect();
+        }
+
+        private void Initialized()
+        {
+            // server argument
+            Instance.TCP = null;
+            Instance.UDP = null;
+            Instance.Address = null;
+            Instance.Port = 0;
+            Instance.BufferSize = new byte[4096];
+            Instance.Receive = 0;
+            // lobby argument
+            Instance.Connected = false;
+            Instance.State = (int)PeerValue.Disconnected;
+            Instance.ID = 0;
+            Instance.Version = null;
+            Instance.Nickname = null;
+        }
+        public virtual void SetConnect() { }
+        public virtual void Connect(string _address, int _port) { Initialized(); }
+        public virtual void Disconnect() { }
+        public virtual void StringSplitsWordsDelimiter(string _text) { }
+        public virtual void ParsePacket(int _length) { }
+        public virtual void Send(string _text) { }
+        public static void WriteLine(string _text) { Console.WriteLine(_text); }
+        public byte[] ShortToByte(int _value)
+        {
+            byte[] temp = new byte[2];
+            temp[1] = (byte)((_value & 0x0000ff00) >> 8);
+            temp[0] = (byte)((_value & 0x000000ff));
+            return temp;
+        }
     }
 }
