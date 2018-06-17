65231bd6ff83f51f37c9e3b45318f26844bb0292
ChoGwanHee <enycal@gmail.com>
2018-06-18 06:26:13 +0900

    Changing user weapon status and implementing user behavior logic developing

---
+++ ServerModule/StateManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerModule
+{
+    public enum GameState
+    {
+        Wait,
+        Playing,
+        Result
+    }
+
+    public static class StateManager
+    {
+        public static int ChangeState(int state)
+        {
+            switch(state)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                case 2:
+                    return 2;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
